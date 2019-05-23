3287f1734ec92be69a0510c346c44fc10fdaa982
Gabriel <gabrielpk.18@gmail.com>
2019-05-23 00:30:17 -0300

    Style: Clean script, smart contract and arapp.json

--- scripts/deploy-tokens.js
+++ scripts/deploy-tokens.js
@@ -1,4 +1,3 @@
-const logDeploy = require('@aragon/os/scripts/helpers/deploy-logger')
 const getAccounts = require('./helpers/get-accounts')
 
 const globalArtifacts = this.artifacts // Not injected unless called directly via truffle
@@ -6,12 +5,10 @@ const globalWeb3 = this.web3 // Not injected unless called directly via truffle
 
 const defaultOwner = process.env.OWNER
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
-
-const getKernelEventLogs = (kernel, event) => {
-  kernel[event]({}, (err, log) => {
-    console.log(log)
-  })
-}
+const PROXY_APP_NAMESPACE =
+  '0xd6f028ca0e8edb4a8c9757ca4fdccab25fa1e0317da1188108f7d2dee14902fb'
+const KERNEL_DEFAULT_VAULT_APP_ID =
+  '0x7e852e0fcfce6551c13800f1e7476f982525c2b5277ba14b24339c68416336d1'
 
 module.exports = async (
   truffleExecCallback,
@@ -36,14 +33,11 @@ module.exports = async (
     )
   }
 
-  const BasicToken = artifacts.require('TestToken')
+  const ERC20Token = artifacts.require('ERC20Token')
+  const Kernel = artifacts.require('Kernel')
+  const Vault = artifacts.require('Vault')
 
-  const valutAddress = '0xd4303618a7eab0092a4bdea064a1be66c665478c'
-
-  log('Deploying BasicTokens...')
-  const token0 = await BasicToken.new(valutAddress, 'TEST', 'TST', 18)
-  //const token1 = await BasicToken.new()
-  //await logDeploy(token0, { verbose })
+  const daoAddress = process.argv.slice(4)[0]
 
   const kernel = await Kernel.at(daoAddress)
   const vaultAddress = await kernel.getApp(
@@ -52,24 +46,20 @@ module.exports = async (
   )
   const vault = Vault.at(vaultAddress)
 
+  // Deposit test tokens
   const token0 = await ERC20Token.new(owner, 'Test Token 0', 'TS0', 18)
   await token0.approve(vaultAddress, 100)
-  const receipt0 = await vault.deposit(token0.address, 100)
+  await vault.deposit(token0.address, 100)
 
-  //token0.transfer(valutAddress, 100)
+  const token1 = await ERC20Token.new(owner, 'Test Token 1', 'TS1', 18)
+  await token1.approve(vaultAddress, 50)
+  await vault.deposit(token1.address, 50)
 
+  // Deposit ETH
   await vault.deposit(ZERO_ADDRESS, 2e18, { value: 2e18 })
 
-  console.log(
-    'Vault token0:',
-    token0.address,
-    await token0.balanceOf(vaultAddress)
-  )
-  console.log(
-    'Vault token1',
-    token1.address,
-    await token1.balanceOf(vaultAddress)
-  )
+  log('Vault token0:', token0.address, await token0.balanceOf(vaultAddress))
+  log('Vault token1', token1.address, await token1.balanceOf(vaultAddress))
 
   if (typeof truffleExecCallback === 'function') {
     // Called directly via `truffle exec`
