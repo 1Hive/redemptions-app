335a97cd0faacb05fb53dac99b8c2937fb713854
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-07-08 15:13:05 -0300

    Add tokens helper

---
+++ scripts/helpers/get-tokens.js
@@ -0,0 +1,22 @@
+const decimals = 18
+
+module.exports = [
+  {
+    name: 'Dai Token',
+    symbol: 'DAI',
+    decimals,
+    amount: 100e18
+  },
+  {
+    name: 'Omise Go',
+    symbol: 'OMG',
+    decimals,
+    amount: 141894e17
+  },
+  {
+    name: 'Maker',
+    symbol: 'MKR',
+    decimals,
+    amount: 40e18
+  }
+]
